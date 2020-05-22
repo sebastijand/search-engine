99783df2c7c47227bb95a8390a52aa536864ad8d
sebastijand <58041623+sebastijand@users.noreply.github.com>
2020-05-22 18:44:25 +0200

    1 Commit

---
+++ search-engine.js
@@ -0,0 +1,10 @@
+// koristit .map za array i koristit mapreduce
+// file text treba provariti (parsati, parseint) i obraditi
+
+
+function suggestionTool(currentText){
+    //
+    //
+}
+
+console.log(suggestionTool("I am"))
