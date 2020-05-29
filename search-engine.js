079b4bda3f186374a6301b082027f349112e2574
sebastijand <58041623+sebastijand@users.noreply.github.com>
2020-05-29 09:57:16 +0200

    3 commit

--- search-engine.js
+++ search-engine.js
@@ -1,4 +1,27 @@
-function searchEngine() {
+const fs = require('fs');
+var text = fs.readFileSync("./long.txt").toString('utf-8');
+
+
+function charFilter(currentText){
+    var currentText = text.replace(/[&\/\\#,+()$~%.'":!*?<>{}^_^]/g, '').split(" "); 
+    return currentText;
 }
 
-module.exports = searchEngine;
+console.log(charFilter())
+
+
+function twoWords(){
+}
+
+
+function threeWords(){    
+}
+
+function fourWords(){
+}
+
+
+module.exports = charFilter;
+module.exports = twoWords;
+module.exports = threeWords;
+module.exports = fourWords;
--- test/search-engine.spec.js
+++ test/search-engine.spec.js
@@ -2,11 +2,53 @@ var assert = require('assert');
 var chai = require("chai");
 var expect = chai.expect;
 var should = chai.should;
-const searchEngine = require("../search-engine");
+const charFilter = require("../search-engine");
+const twoWords = require("../search-engine");
+const threeWords = require("../search-engine")
+const fourWords = require("../search-engine")
 
 
 describe('search engine', function() {
-    it('searchEngine treba biti funckcija', function () {
-        searchEngine();
+    it('charFilter must be a function', function () {
+        charFilter();
     });
+});
+
+
+
+describe('two words', function() {
+    it('twoWords must be a function', function () {
+        twoWords();
+    });
+    /*it("For first word, funcition returns suggestion for next word", function() {
+        let result = twoWords( ... );
+        expect(twoWords).to.eq("...");
+    });
+    */
+});
+
+
+
+describe('three words', function() {
+    it('threeWords must be a function', function () {
+        threeWords();
+    });
+    /*it("For second word, funcition returns suggestion for next word", function() {
+        let result = threeWords(...);
+        expect(threeWords[1]).to.eq("...");
+    });
+    */
+});
+
+
+
+describe('four words', function() {
+    it('fourWords must be a function', function () {
+        fourWords();
+    });
+    /*it("For third or more words, function must not return suggestion", function() {
+        let result = fourWords(...);
+        assert.strictEqual(result, undefined);
+    });
+    */
 });
